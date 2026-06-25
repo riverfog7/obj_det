520e0a57216b8862a6ea4254fb5abeb8aed26c07
cjw9770@gmail.com <cjw9770@gmail.com>
2026-06-25 16:06:28 +0000

    feat: add visdrone dataset download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -9,7 +9,15 @@ HAZYDET_TEST_ZIP="test.zip"
 HAZYDET_VAL_ZIP="val.zip"
 HAZYDET_REAL_WORLD_ZIP="real_world.zip"
 
-rm -rf "$DATASET_SAVE_PATH
+VISDRONE_SAVE_PATH="$DATASET_SAVE_PATH/visdrone"
+VISDRONE_TRAIN_URL="https://drive.google.com/file/d/1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn/view?usp=sharing"
+VISDRONE_TEST_URL="https://drive.google.com/open?id=1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V"
+VISDRONE_VAL_URL="https://drive.google.com/file/d/1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59/view?usp=sharing"
+VISDRONE_TRAIN_ZIP="VisDrone2019-DET-train.zip"
+VISDRONE_VAL_ZIP="VisDrone2019-DET-val.zip"
+VISDRONE_TEST_ZIP="VisDrone2019-DET-test-dev.zip"
+
+rm -rf "$DATASET_SAVE_PATH"
 
 echo "Downloading hazydet dataset..."
 mkdir -p "$HAZYDET_SAVE_PATH"
@@ -29,4 +37,20 @@ rm "$HAZYDET_TRAIN_ZIP"
 rm "$HAZYDET_VAL_ZIP"
 rm "$HAZYDET_TEST_ZIP"
 rm "$HAZYDET_REAL_WORLD_ZIP"
+popd
+
+echo "Downloading visdrone dataset..."
+mkdir -p "$VISDRONE_SAVE_PATH"
+pushd "$VISDRONE_SAVE_PATH"
+uv tool run gdown "$VISDRONE_TEST_URL"
+uv tool run gdown "$VISDRONE_TRAIN_URL"
+uv tool run gdown "$VISDRONE_VAL_URL"
+
+echo "Extracting visdrone dataset..."
+unzip -q "$VISDRONE_TRAIN_ZIP"
+unzip -q "$VISDRONE_VAL_ZIP"
+unzip -q "$VISDRONE_TEST_ZIP" -d "VisDrone2019-DET-test-dev"
+rm "$VISDRONE_TEST_ZIP"
+rm "$VISDRONE_TRAIN_ZIP"
+rm "$VISDRONE_VAL_ZIP"
 popd
